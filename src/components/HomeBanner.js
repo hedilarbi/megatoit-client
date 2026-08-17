94a643f89f9cf953fc98a589f606b5fc078b41f9
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-08-17 15:36:14 +0100

    12

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -8,14 +8,14 @@ const HomeBanner = () => {
       <section className="relative">
         <div className={styles.heroBanner}>
           <div>
-            <h1 className="font-bebas-neue md:text-7xl text-5xl text-white text-center tracking-wider">
+            <h1 className="font-bebas-neue text-3xl md:text-7xl text-white text-center tracking-wider">
               BILLETTERIE en ligne
             </h1>
 
-            <div className="flex flex-col md:flex-row md:gap-8 gap-4 md:justify-center items-center mt-10">
+            <div className="flex flex-col md:flex-row md:gap-8 gap-4 md:justify-center items-center mt-6 md:mt-10">
               <Link
                 href="/calendrier"
-                className="border-2 border-brand text-black px-12 py-3 text-2xl rounded-lg bg-brand hover:bg-brand-dark transition-colors shadow-lg"
+                className="border-2 border-brand text-black px-8 py-2 md:px-12 md:py-3 text-lg md:text-2xl rounded-lg bg-brand hover:bg-brand-dark transition-colors shadow-lg"
               >
                 <span className="font-bebas-neue text-center tracking-wide">
                   Billets de match
@@ -24,7 +24,7 @@ const HomeBanner = () => {
               <Link
                 href="https://megatoit.atelierqg.com/"
                 target="_blank"
-                className="border-2 border-brand text-brand hover:text-black px-12 py-3 text-2xl rounded-lg bg-black hover:bg-brand transition-colors shadow-lg"
+                className="border-2 border-brand text-brand hover:text-black px-8 py-2 md:px-12 md:py-3 text-lg md:text-2xl rounded-lg bg-black hover:bg-brand transition-colors shadow-lg"
               >
                 <span className="font-bebas-neue text-center tracking-wide">
                   Boutique en ligne
